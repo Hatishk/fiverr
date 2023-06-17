f9e518aad0e62ebc05afbcddcf36408b76754a1d
Hatish <himanshuasija786@gmail.com>
2023-06-18 01:19:49 +0530

    removed unwanted pages

--- src/pages/add/Add.jsx
+++ src/pages/add/Add.jsx
@@ -4,7 +4,49 @@ import "./Add.scss"
 
 const Add = () => {
     return (
-        <div className='add'>Add</div>
+        <div className='add'>
+            <div className="container">
+                <h1>Add New Gig</h1>
+                <div className="sections">
+                    <div className="left">
+                        <label htmlFor="">Title</label>
+                        <input type="text" placeholder='e.g I am really good at' />
+                        <label htmlFor="">Category</label>
+                        <select name="cats" id="cats">
+                            <option value="design">Design</option>
+                            <option value="Web">Web Development</option>
+                            <option value="animation">Animation</option>
+                            <option value="writer">Content Writer</option>
+                        </select>
+                        <label htmlFor="">Cover Image</label>
+                        <input type="file" />
+                        <label htmlFor="">Upload Images</label>
+                        <input type="file" multiple />
+                        <label htmlFor="">Description</label>
+                        <textarea name="" id="" cols="30" rows="16" placeholder='share a bit about your work experience, cool projects you’ve completed, and your area of expertise.'>  
+                        </textarea>
+                        <button>Create</button>
+                    </div>
+                    <div className="right">
+                        <label htmlFor="">Service Title</label>
+                        <input type="text" placeholder='e.g. I Build Things On Web' />
+                        <label htmlFor="">Short Description</label>
+                        <textarea name="" id="" cols="30" rows="10" placeholder='Short description about your service'></textarea>
+                        <label htmlFor="">Delivery Time(e.g. 3 Days)</label>
+                        <input type="number" min={1} />
+                        <label htmlFor="">Revision Number</label>
+                        <input type="number" min={1} />
+                        <label htmlFor="">Add Features</label>
+                        <input type="text" placeholder='e.g. page design' />
+                        <input type="text" placeholder='e.g. file uploading' />
+                        <input type="text" placeholder='e.g setting up a domain' />
+                        <input type="text" placeholder=' e.g. hosting' />
+                        <label htmlFor="">Price</label>
+                        <input type="number" min={1} />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/pages/messages/Messages.jsx
+++ src/pages/messages/Messages.jsx
@@ -1,9 +1,89 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import './Messages.scss'
+
 
 const Messages = () => {
+  const currentUser = {
+    id: 1,
+    username: "Anna",
+    isSeller: true,
+  };
+
+  const message = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+  maxime cum corporis esse aspernatur laborum dolorum? Animi
+  molestias aliquam, cum nesciunt, aut, ut quam vitae saepe repellat
+  nobis praesentium placeat.`;
+
   return (
-    <div>Messages</div>
-  )
-}
+    <div className="messages">
+      <div className="container">
+        <div className="title">
+          <h1>Messages</h1>
+        </div>
+        <table>
+          <tr>
+            <th>{currentUser.isSeller ? "Buyer" : "Seller"}</th>
+            <th>Last Message</th>
+            <th>Date</th>
+            <th>Action</th>
+          </tr>
+          <tr className="active">
+            <td>Osama Laden</td>
+            <td>
+              <Link to="/singlemessage/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 hour ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr className="active">
+            <td>Elon Musk</td>
+
+            <td>
+              <Link to="/singlemessage/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>2 hours ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr>
+            <td>Narendra Modi</td>
+            <td>
+              <Link to="/singlemessage/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 day ago</td>
+          </tr>
+          <tr>
+            <td>Tanmay Bhatt </td>
+            <td>
+              <Link to="/singlemessage/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>2 days ago</td>
+          </tr>
+          <tr>
+            <td>Code Harry</td>
+            <td>
+            <Link to="/singlemessage/123" className="link">
+              {message.substring(0, 100)}...
+              </Link>
+              </td>
+            <td>1 week ago</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
 
 export default Messages
